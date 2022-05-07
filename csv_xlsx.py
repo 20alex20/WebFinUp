073d4f02f99e659ba24f7fd8a50d1fd4bcb84672
alex <mike123777mike@gmail.com>
2022-05-07 23:47:38 +0300

    Добавлено три страницы и экспорт

--- csv_xlsx.py
+++ csv_xlsx.py
@@ -2,7 +2,7 @@ import csv
 import xlsxwriter
 from main import get_all_data
 from main import environ
-
+from zipfile import ZipFile
 
 headers2 = {
     "categories": ("id_category", "name", "description"),
@@ -11,10 +11,10 @@ headers2 = {
     "bank_accounts": ("id_bank_account", "name", "current_sum", "description"),
     "deposits":  ("id_deposit", "id_category", "id_bank_account", "sum", "date", "comment"),
 }
-d = 'session_files'
+d = 'static/export'
 
 
-def export_csv():
+def export_csv_2():
     for name, cur_list in zip(sorted(headers2), get_all_data()):
         headers = headers2[name]
         with open(d + f'/export_data_FinUp_{name}.csv', 'w', newline='') as csvfile:
@@ -24,15 +24,26 @@ def export_csv():
     return "Осуществлен экспорт"
 
 
-def export_xlsx():  # directory - Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+def export_xlsx():
     workbook = xlsxwriter.Workbook(d + '/export_data_FinUp.xlsx')
     for name, cur_list in zip(sorted(headers2), get_all_data()):
         headers = headers2[name]
-        worksheet = workbook.add_worksheet()
+        worksheet = workbook.add_worksheet(name=name)
         for column, i in enumerate(headers):
             worksheet.write(0, column, i)
         for row, i in enumerate(cur_list):
             for column, j in enumerate(i):
                 worksheet.write(row + 1, column, j)
     workbook.close()
+    return "Осуществлен экспорт"
+
+
+def export_csv():
+    with ZipFile(d + '/export_data_FinUp.zip', 'w') as myzip:
+        for name, cur_list in zip(sorted(headers2), get_all_data()):
+            headers = headers2[name]
+            string = ';'.join(headers) + '\n'
+            for i in cur_list:
+                string += ';'.join(map(str, i)) + '\n'
+            myzip.writestr(f'export_data_FinUp_{name}.csv', string.encode('windows-1251'))
     return "Осуществлен экспорт"
--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, SelectField, DateField
 from wtforms.validators import DataRequired
 from flask import Flask, url_for, render_template, redirect, make_response
+from csv_xlsx import *
 
 from main import *
 import json
@@ -41,8 +42,19 @@ class Alert:
         self.alert = s
 
 
+def generate_params(title, **kwargs):
+    params = {
+        "alert": alert.get(),
+        "username": get_full_name(),
+        "title": title
+    }
+    for keys, value in kwargs.items():
+        params[keys] = value
+    return params
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'FinUp'
+app.config['SECRET_KEY'] = 'WebFinUp'
 alert = Alert()
 
 
@@ -82,7 +94,7 @@ def register_():
             return render_template('register.html', form=form, alert=alert.get())
         else:
             return redirect('/index')
-    return render_template('register.html', form=form, alert=alert.get())
+    return render_template('register.html', form=form, )
 
 
 @app.route('/logout')
@@ -114,8 +126,8 @@ def index():
     summa = get_sum()
     income = get_sum_deposits()
     expense = get_sum_purchases()
-    full_name = get_full_name()
-    return render_template('index.html', alert=alert.get(), summa=summa, income=income, expense=expense, username=full_name)
+    params = generate_params("Панель управления", summa=summa, income=income, expense=expense)
+    return render_template('index.html', **params)
 
 
 @app.route('/success')
@@ -127,6 +139,12 @@ def success():
 def widgets():
     full_name = get_full_name()
     return render_template('widgets.html', username=full_name)
+
+
+@app.route('/icons')
+def icons():
+    full_name = get_full_name()
+    return render_template('icons.html', username=full_name)
 
 
 @app.route('/tables')
@@ -146,8 +164,8 @@ class Income(FlaskForm):
     bank_accounts_ = [(str(i[0]), i[1], str(i[2])) for i in get_bank_accounts()]
     categories = SelectField('Категория', validators=[DataRequired()], choices=[i[1] for i in categories_])
     bank_accounts = SelectField('Счет', validators=[DataRequired()], choices=[i[1] + ' (' + i[2] + '₽)' for i in bank_accounts_])
-    comment = StringField('Комментарий', validators=[DataRequired()])
-    sum = EmailField('Сумма', validators=[DataRequired()])
+    comment = StringField('Комментарий')
+    sum = StringField('Сумма', validators=[DataRequired()])
     date = DateField('Дата', validators=[DataRequired()])
     submit = SubmitField('Добавить')
 
@@ -169,8 +187,8 @@ def income():
         alert.put(add_deposit(category, bank_account, form.sum.data, form.date.data.strftime("%d.%m.%Y"), form.comment.data))
         if alert.read() == "Данные изменены":
             return redirect('/index')
-    full_name = get_full_name()
-    return render_template('purchase.html', username=full_name, name="Доход", alert=alert.get(), form=form)
+    params = generate_params("Добавить", name="Доход", form=form)
+    return render_template('purchase.html', **params)
 
 
 class Expense(FlaskForm):
@@ -178,8 +196,8 @@ class Expense(FlaskForm):
     bank_accounts_ = [(str(i[0]), i[1], str(i[2])) for i in get_bank_accounts()]
     categories = SelectField('Категория', validators=[DataRequired()], choices=[i[1] for i in categories_])
     bank_accounts = SelectField('Счет', validators=[DataRequired()], choices=[i[1] + ' (' + i[2] + '₽)' for i in bank_accounts_])
-    comment = StringField('Комментарий', validators=[DataRequired()])
-    sum = EmailField('Сумма', validators=[DataRequired()])
+    comment = StringField('Комментарий')
+    sum = StringField('Сумма', validators=[DataRequired()])
     date = DateField('Дата', validators=[DataRequired()])
     submit = SubmitField('Добавить')
 
@@ -202,8 +220,28 @@ def expense():
                                form.date.data.strftime("%d.%m.%Y"), form.comment.data))
         if alert.read() == "Данные изменены":
             return redirect('/index')
-    full_name = get_full_name()
-    return render_template('purchase.html', username=full_name, name="Расход", alert=alert.get(), form=form)
+    params = generate_params("Добавить", name="Расход", form=form)
+    return render_template('purchase.html', **params)
+
+
+@app.route('/bank_accounts', methods=['GET', 'POST'])
+def bank_accounts():
+    with open(session_file, "r") as f:
+        if f.read() == '':
+            return redirect('/login')
+
+    data = []
+    for i in get_bank_accounts():
+        d = {
+            "name": i[1],
+            "sum": str(i[2])
+        }
+        data.append(d)
+    with open('static/tables/data1.json', 'w+') as file:
+        json.dump(data, file, indent=4)
+
+    params = generate_params("Счета")
+    return render_template('bank_accounts.html', **params)
 
 
 @app.route('/categories', methods=['GET', 'POST'])
@@ -228,8 +266,8 @@ def categories():
     with open('static/tables/data2.json', 'w+') as file:
         json.dump(data, file, indent=4)
 
-    full_name = get_full_name()
-    return render_template('categories.html', username=full_name, name="Расход", alert=alert.get())
+    params = generate_params("Категории")
+    return render_template('categories.html', **params)
 
 
 class IncomeCategory(FlaskForm):
@@ -237,7 +275,7 @@ class IncomeCategory(FlaskForm):
     categories_.insert(0, ('-1', 'Новая категория'))
     categories = SelectField('Категория', validators=[DataRequired()], choices=[i[1] for i in categories_])
     name = StringField('Название', validators=[DataRequired()])
-    comment = StringField('Описание', validators=[DataRequired()])
+    comment = StringField('Описание')
     submit = SubmitField('Добавить/Изменить')
 
 
@@ -248,9 +286,7 @@ def income_category():
             return redirect('/login')
 
     form = IncomeCategory()
-    print(form.validate_on_submit())
     if form.validate_on_submit():
-        print(form.categories.data)
         if form.categories.data == 'Новая категория':
             alert.put(add_deposit_category(form.name.data, form.comment.data))
         else:
@@ -260,8 +296,81 @@ def income_category():
             alert.put(edit_deposit_category(category, form.name.data, form.comment.data))
         if alert.read() == "Данные изменены":
             return redirect('/index')
-    full_name = get_full_name()
-    return render_template('category.html', username=full_name, name="Доход", alert=alert.get(), form=form)
+    params = generate_params("Добавить", name="Доход", form=form)
+    return render_template('category.html', **params)
+
+
+class ExpensesCategory(FlaskForm):
+    categories_ = [(str(i[0]), i[1]) for i in get_categories()]
+    categories_.insert(0, ('-1', 'Новая категория'))
+    categories = SelectField('Категория', validators=[DataRequired()], choices=[i[1] for i in categories_])
+    name = StringField('Название', validators=[DataRequired()])
+    comment = StringField('Описание')
+    submit = SubmitField('Добавить/Изменить')
+
+
+@app.route('/expenses_category', methods=['GET', 'POST'])
+def expenses_category():
+    with open(session_file, "r") as f:
+        if f.read() == '':
+            return redirect('/login')
+
+    form = ExpensesCategory()
+    if form.validate_on_submit():
+        if form.categories.data == 'Новая категория':
+            alert.put(add_category(form.name.data, form.comment.data))
+        else:
+            for i in form.categories_:
+                if i[1] == form.categories.data:
+                    category = i[0]
+            alert.put(edit_category(category, form.name.data, form.comment.data))
+        if alert.read() == "Данные изменены":
+            return redirect('/index')
+    params = generate_params("Добавить", name="Расход", form=form)
+    return render_template('category.html', **params)
+
+
+class BankAccounts(FlaskForm):
+    bank_accounts_ = [(str(i[0]), i[1]) for i in get_categories()]
+    bank_accounts_.insert(0, ('-1', 'Новый счет'))
+    bank_accounts = SelectField('Счет', validators=[DataRequired()], choices=[i[1] for i in bank_accounts_])
+    name = StringField('Название', validators=[DataRequired()])
+    sum = StringField('Сумма', validators=[DataRequired()], default='0')
+    comment = StringField('Описание')
+    submit = SubmitField('Добавить/Изменить')
+
+
+@app.route('/bank_account', methods=['GET', 'POST'])
+def bank_account():
+    with open(session_file, "r") as f:
+        if f.read() == '':
+            return redirect('/login')
+
+    form = BankAccounts()
+    if form.validate_on_submit():
+        if form.bank_accounts.data == 'Новый счет':
+            alert.put(add_category(form.name.data, form.comment.data))
+        else:
+            for i in form.bank_accounts_:
+                if i[1] == form.bank_accounts.data:
+                    bank_account = i[0]
+            alert.put(edit_category(bank_account, form.name.data, form.comment.data))
+        if alert.read() == "Данные изменены":
+            return redirect('/index')
+    params = generate_params("Добавить", form=form)
+    return render_template('bank_account.html', **params)
+
+
+@app.route('/export_xlsx')
+def export_xlsx_():
+    export_xlsx()
+    return redirect('/static/export/export_data_FinUp.xlsx')
+
+
+@app.route('/export_csv')
+def export_csv_():
+    export_csv()
+    return redirect('/static/export/export_data_FinUp.zip')
 
 
 @app.errorhandler(404)
